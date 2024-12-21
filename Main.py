617eaa320b547da752fa2ee8bfd2fd069119b67a
mazyar ghezelji <maz.ghezelji@gmail.com>
2024-12-21 15:56:19 -0800

    adding the files

---
+++ Main.py
@@ -0,0 +1,11 @@
+#%%
+import pandas as pd
+import numpy as np
+
+#%% 
+data_path = 'Steam_2024_bestRevenue_1500.csv'
+pd.read_csv(data_path)
+
+# %%
+# make a structure to analysie the dataset
+# have a checklist
